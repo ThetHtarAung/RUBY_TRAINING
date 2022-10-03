935fd4d3a3012e3e8e28309acb338838c3045bb3
thethtargithub <thethtaraung1999@gmail.com>
2022-10-03 13:56:26 +0630

    fixed tutorial6 review

--- Tutorial6/tutorial6.rb
+++ Tutorial6/tutorial6.rb
@@ -4,16 +4,19 @@ puts "Enter notes"
 note = gets.chomp()
 puts "Want to save/cancel?"
 decision = gets.chomp()
-if decision == "save"
+
+if decision === "save"
   File.open('note.txt', 'w') do |f|   
     f.syswrite(title)   
     f.syswrite(note)
   end
   puts "Want to open (y/n): "
   open = gets.chomp()
-  if open == "y"
+
+  if open === "y"
     puts "File name (filename.txt):"
     file_name = gets.chomp()
+    
     if File.exists?(file_name)
       aFile = File.new(file_name, "r")   
       content = aFile.read()   
@@ -22,10 +25,6 @@ if decision == "save"
       puts "file not found"
     end  
   end  
-
 else
   puts "Cancel"
 end
-
-
-  
