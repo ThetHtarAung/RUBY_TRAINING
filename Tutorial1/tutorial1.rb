cf38c0dd86dccb6ec14b6d0b1f7b2d03769f91ae
thethtargithub <thethtaraung1999@gmail.com>
2022-10-03 13:07:01 +0630

    update tutorial1 review

--- Tutorial1/tutorial1.rb
+++ Tutorial1/tutorial1.rb
@@ -1,12 +1,12 @@
-space=" "
-space_count= 6
+space = " "
+space_count = 6
+
 11.times do |row|
 
   if row < 6
     space_count -=1
     star = row *2 +1
     print space * space_count
-
   else 
     space_count += 1
     star = (11-1-row)*2 +1
@@ -14,4 +14,3 @@ space_count= 6
   end
   puts '*' * star
 end
-
