6cf2d57a0977a02ccb6f27e72ed341c0147c1fb6
thethtargithub <thethtaraung1999@gmail.com>
2022-10-03 13:51:33 +0630

    fixed tutorial5 review

--- Tutorial5/tutorial5.rb
+++ Tutorial5/tutorial5.rb
@@ -1,30 +1,28 @@
 require 'date'
-def return_name (d_num)
-  if d_num == 0
+def return_name (day_num)
+  if day_num == 0
     print "Sunday,"
-  elsif d_num == 1
+  elsif day_num == 1
     print "Monday,"
-  elsif d_num == 2
+  elsif day_num == 2
     print "Tuesday,"
-  elsif d_num == 3
+  elsif day_num == 3
     print "Wednesday,"
-  elsif d_num == 4
+  elsif day_num == 4
     print "Thursday,"
-  elsif d_num == 5
+  elsif day_num == 5
     print "Friday,"
-  elsif d_num == 6
+  elsif day_num == 6
     print "Saturday,"
   end
 end
-
 
 current_date = Date.today
 puts "Current day:"
 puts return_name(current_date.wday)
 puts "Last 5 days:"
+
 5.times do
   current_date = current_date.prev_day
   return_name(current_date.wday)
 end
-
-
