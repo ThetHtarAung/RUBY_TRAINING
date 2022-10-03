8e518a55519b683a525dbee7e8748d5fa687e96f
thethtargithub <thethtaraung1999@gmail.com>
2022-10-03 13:21:36 +0630

    fixed tutorial2 review

--- Tutorial2/tutorial2.rb
+++ Tutorial2/tutorial2.rb
@@ -4,9 +4,9 @@ date_array = date.split '/'
 time = Time.new
 current_year = time.year
 age = current_year - date_array[2].to_i
+
 if age < 19
   puts "#{age},you are a child"
 else
   puts "#{age},you are an adult"
 end
-
