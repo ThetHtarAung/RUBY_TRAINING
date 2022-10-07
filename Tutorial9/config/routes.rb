684cc6435d955f4bfaff6a10f6beb898408d3837
thethtargithub <thethtaraung1999@gmail.com>
2022-10-07 08:15:05 +0630

    delete tutorial9 folder

--- Tutorial9/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "tutorial9#index"
-  get "/tutorial9", to: "tutorial9#index"
-  post "/tutorial9", to: "tutorial9#upload"
-end
