31fe1c5fa309d6d6e1f43b4cc893059ad26f515c
thethtargithub <thethtaraung1999@gmail.com>
2022-10-11 19:39:44 +0630

    Tutorial10

--- Tutorial10/app/controllers/tutorial10_controller.rb
+++ Tutorial10/app/controllers/tutorial10_controller.rb
@@ -1,5 +1,6 @@
 class Tutorial10Controller < ApplicationController
   require 'roo'
+  require 'docx'
   def index
   end
   def upload
@@ -15,6 +16,22 @@ class Tutorial10Controller < ApplicationController
       afile.each do |row|
       file_data << row
       end
+    elsif file_type === "xls"
+      afile = Roo::Excel.new("./app/assets/files/#{file_name}.#{file_type}")
+      afile.each do |row|
+      file_data << row
+      end
+    elsif file_type === "csv"
+      afile = Roo::Csv.new("./app/assets/files/#{file_name}.#{file_type}")
+      afile.each do |row|
+      file_data << row
+      end
+    elsif file_type === "docx"
+      afile = Docx::Document.open("./app/assets/files/#{file_name}.#{file_type}") 
+      afile.paragraphs.each do |p|
+        file_data << p
+      end
+      
     end
     flash[:notice] = file_data
     redirect_to tutorial10_url
